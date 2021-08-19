319f9445e1028c6df8b3cd18029120e88a8408e5
Rishi Bansal <rishibansal02@gmail.com>
2021-08-19 20:44:03 +0530

    .

--- case_study_6.py
+++ case_study_6.py
@@ -20,6 +20,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 import numpy as np
+from sklearn.feature_selection import SelectKBest, f_classif
 #Implement through pipeline
 #pipeline_1 = age, fare - SimpleImputer , StandardScaler
 #pipeline_2 = sex, embarked - SimpleImpputer, OnehoTencoder
@@ -33,7 +34,7 @@ preprocessor = make_column_transformer(
     )
 
 
-master_pipeline = make_pipeline(preprocessor, DecisionTreeClassifier())
+master_pipeline = make_pipeline(preprocessor, SelectKBest(k=3, score_func=f_classif),DecisionTreeClassifier())
 
 
 #split into train and test set
@@ -44,6 +45,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 master_pipeline.fit(X_train, y_train)
 #calculate the score
 print(master_pipeline.score(X_test, y_test))
+
+
+from sklearn.model_selection import GridSearchCV
+param = {'selectkbest__k': [2, 3, 4,5,6]}
+gs = GridSearchCV(master_pipeline, param_grid=param, cv=5)
+gs.fit(X_train, y_train)
+print(gs.best_score_)
 
 
 
